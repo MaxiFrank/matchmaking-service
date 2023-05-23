92ad554c28d8ed86db319362afba5ab36640a2e9
MaxiFrank <maxine.m.liu@gmail.com>
2023-05-23 15:20:32 -0700

    set up initial landing page

---
+++ server.py
@@ -0,0 +1,17 @@
+"""Server for movie ratings app."""
+
+from flask import Flask, render_template
+import os
+
+
+app = Flask(__name__)
+app.secret_key = "dev"
+
+
+@app.route("/")
+def homepage():
+    """View homepage."""
+    return render_template("homepage.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
